779769fc6da9ac911c913ca4408957fe81cf09f9
jflynn129 <jflynn129@gmail.com>
2018-03-28 14:52:33 -0400

    Modified usage by creating a MQTT_USE macro to specify the Network Interface.

--- MQTT.h
+++ MQTT.h
@@ -16,31 +16,8 @@ class MQTTconnect : public MQTTSocket
     { return eth; }
 };
 
-//
-// To add additional transports, Define it as:
-//
-#define MQTT_BG96         100
-#define MQTT_WNC14A2A     101
-#define MQTT_ETHERNET     102
-
-//
-// Then define the interface that will be used:
-//
-#if   MBED_CONF_APP_MQTT_INTERFACE == MQTT_BG96
-#define _ift BG96Interface
-
-#elif MBED_CONF_APP_MQTT_INTERFACE == MQTT_WNC14A2A
-#define _ift WNC14A2AInterface
-
-#elif MBED_CONF_APP_MQTT_INTERFACE == MQTT_ETHERNET
-#define _ift EthernetInterface
-
-#else
-#error "No MQTT Network Transport chosen. Please add 'config.mqtt-interfaces.value' to your mbed_app.json (see README.md for more information)."
-#endif
-
-typedef MQTTconnect<_ift> MQTTct; 
-typedef _ift MQTTnet;
+#define MQTT_USE(x)	typedef MQTTconnect<x> MQTTct; typedef x MQTTnet;
 
 #endif
+
 
